c4125c5ffd1e940111183dbbb1125d31a0e239db
MDSHOHAG617 <mdshohag119764@gmail.com>
2022-09-26 02:59:52 +0600

    blog added

--- src/Components/Home/Blogs/Blogs.js
+++ src/Components/Home/Blogs/Blogs.js
@@ -5,6 +5,8 @@ import blog2 from "../../../images/blog2.png";
 import blog3 from "../../../images/blog3.png";
 import streems from "../../../images/streem.png";
 import press from "../../../images/press.png";
+import user1 from "../../../images/user1.png";
+import user2 from "../../../images/user2.png";
 
 const Blogs = () => {
   return (
@@ -26,6 +28,35 @@ const Blogs = () => {
         <img className="streems-1" src={streems} alt="" />
         <img className="streems-2" src={streems} alt="" />
         <img className="streems-3" src={press} alt="" />
+      </div>
+      <div className="blogs-by-user">
+        <div className="users users-1">
+          <div className="user">
+            <img src={user1} alt="" />
+            <p className="blog-role">June 31th by Admin</p>
+          </div>
+          <p className="user-comment">
+            Lorem Ipsum is simply dummy text.Lorem Ipsum is simply dummy text.
+          </p>
+        </div>
+        <div className="users users-2">
+          <div className="user">
+            <img src={user2} alt="" />
+            <p className="blog-role">June 31th by Admin</p>
+          </div>
+          <p className="user-comment">
+            Lorem Ipsum is simply dummy text.Lorem Ipsum is simply dummy text.
+          </p>
+        </div>
+        <div className="users users-3">
+          <div className="user">
+            <img src={user1} alt="" />
+            <p className="blog-role">June 31th by Admin</p>
+          </div>
+          <p className="user-comment">
+            Lorem Ipsum is simply dummy text.Lorem Ipsum is simply dummy text.
+          </p>
+        </div>
       </div>
     </div>
   );
